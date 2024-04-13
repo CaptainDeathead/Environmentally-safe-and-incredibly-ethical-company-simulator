828e7cc3a85361ee497f5557b0835d0026827427
CaptainDeathead <unstableplazma@gmail.com>
2024-04-13 09:17:55 +0800

    Replaced + character with . character so its easier to understand

--- data.py
+++ data.py
@@ -6,15 +6,15 @@ SEED = 12346
 DEPOSIT_TYPES = ['none', 'fossil', 'coal', 'oil', 'gas']
 
 DEPOSIT_SPAWN_CHANCES = { # higher is better
-    'none': 40,
-    'fossil': 15,
+    'none': 100,
+    'fossil': 10,
     'coal': 5,
     'oil': 3,
     'gas': 1
 }
 
 SYMBOLS = {
-    'none': '+',
+    'none': '.',
     'fossil': 'f',
     'coal': 'c',
     'oil': 'o',
