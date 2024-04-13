8bad1db74a6cb2338469fc4ddef7f3e3ad5f023d
CaptainDeathead <unstableplazma@gmail.com>
2024-04-13 12:07:45 +0800

    Added draw method to wire

--- Construction/construction.py
+++ Construction/construction.py
@@ -3,3 +3,7 @@ from typing import List
 class Construction:
     def __init__(self):
         self.wires: List = []
+        self.chunk_connectors: List = []
+        self.hubs: List = []
+        self.extractors: List = []
+        self.generators: List = []
