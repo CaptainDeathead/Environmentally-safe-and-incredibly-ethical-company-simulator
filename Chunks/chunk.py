a7e32c7738e2d143dcb69c1738ab89d04b8930f5
CaptainDeathead <unstableplazma@gmail.com>
2024-04-13 10:18:13 +0800

    Added deposit surrounding

--- Chunks/chunk.py
+++ Chunks/chunk.py
@@ -2,6 +2,7 @@ from typing import List, Tuple
 from utils import unique_number_from_pair
 from data import *
 from random import seed, choices
+from copy import deepcopy
 
 class Chunk:
     def __init__(self, location: Tuple, chunk_id: int):
@@ -17,6 +18,19 @@ class Chunk:
             self.chunk_map.append([])
             for x in range(CHUNK_SIZE):
                 self.chunk_map[y].append(self.get_cell_type((x+1)*(y+1)+self.chunk_seed))
+
+        new_chunk_map: List = deepcopy(self.chunk_map)
+
+        for y in range(CHUNK_SIZE):
+            for x in range(CHUNK_SIZE):
+                if self.chunk_map[y][x] != SYMBOLS['none']:
+                    for y1 in range(-2, 2):
+                        if y+y1 > CHUNK_SIZE-1: continue
+                        for x1 in range(-2, 2):
+                            if x+x1 > CHUNK_SIZE-1: continue
+                            new_chunk_map[y+y1][x+x1] = new_chunk_map[y][x]
+
+        self.chunk_map = deepcopy(new_chunk_map)
                 
     # Takes in a large integer and checks if any deposits are spawned and returns a string corresponding to the deposit
     def get_cell_type(self, cell_num: int) -> str:
--- data.py
+++ data.py
@@ -6,11 +6,11 @@ SEED = 314159
 DEPOSIT_TYPES = ['none', 'fossil', 'coal', 'oil', 'gas']
 
 DEPOSIT_SPAWN_CHANCES = { # higher is better
-    'none': 100,
-    'fossil': 10,
-    'coal': 5,
-    'oil': 3,
-    'gas': 1
+    'none': 400,
+    'fossil': 20,
+    'coal': 10,
+    'oil': 6,
+    'gas': 2
 }
 
 SYMBOLS = {
