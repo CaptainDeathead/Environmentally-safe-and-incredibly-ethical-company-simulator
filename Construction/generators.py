b7447bd31818830bc3cb4e0f9e01a2bb8817ed2c
CaptainDeathead <unstableplazma@gmail.com>
2024-04-16 11:16:04 +0800

    Added temperary resource generators to data

--- Construction/generators.py
+++ Construction/generators.py
@@ -16,3 +16,38 @@ class WindTurbine:
         self.connected: bool = False
         self.DEPENDS_ON_EXTRACTOR: bool = False
         self.GENERATOR_TYPE: str = 'wind_turbine'
+
+
+# --------------------------- THIS CODE WILL BE MOVED TO EXTRACTORS LATER AND AN EXTRACTOR WILL NEED ITS CORROSPONDING GENERATOR TO FUNCTION ---------------------------
+
+class FossilGenerator:
+    def __init__(self, chunk_id: int, location: Tuple):
+        self.chunk_id = chunk_id
+        self.location = location
+        self.connected: bool = False
+        self.DEPENDS_ON_EXTRACTOR: bool = False
+        self.GENERATOR_TYPE: str = 'fossil_generator'
+
+class CoalGenerator:
+    def __init__(self, chunk_id: int, location: Tuple):
+        self.chunk_id = chunk_id
+        self.location = location
+        self.connected: bool = False
+        self.DEPENDS_ON_EXTRACTOR: bool = False
+        self.GENERATOR_TYPE: str = 'coal_generator'
+
+class OilGenerator:
+    def __init__(self, chunk_id: int, location: Tuple):
+        self.chunk_id = chunk_id
+        self.location = location
+        self.connected: bool = False
+        self.DEPENDS_ON_EXTRACTOR: bool = False
+        self.GENERATOR_TYPE: str = 'oil_generator'
+
+class GasGenerator:
+    def __init__(self, chunk_id: int, location: Tuple):
+        self.chunk_id = chunk_id
+        self.location = location
+        self.connected: bool = False
+        self.DEPENDS_ON_EXTRACTOR: bool = False
+        self.GENERATOR_TYPE: str = 'gas_generator'
--- data.py
+++ data.py
@@ -32,13 +32,21 @@ SHOP_ITEMS = ['wire', 'solar_panel', 'wind_turbine']
 SHOP_ITEM_PRICES = {
     'wire': 0,
     'solar_panel': 5000,
-    'wind_turbine': 15000
+    'wind_turbine': 15000,
+    'fossil_generator': 40_000,
+    'coal_generator': 100_000,
+    'oil_generator': 220_000,
+    'gas_generator': 500_000
 }
 
 SHOP_ITEM_TYPES = {
     'wire': 0,
     'solar_panel': 0,
-    'wind_turbine': 0
+    'wind_turbine': 0,
+    'fossil_generator': 0,
+    'coal_generator': 0,
+    'oil_generator': 0,
+    'gas_generator': 0
 }
 
 SHOP_ITEM_TYPES_TO_STR = {
@@ -47,5 +55,9 @@ SHOP_ITEM_TYPES_TO_STR = {
 
 POWER_OUTPUTS = {
     'solar_panel': 250,
-    'wind_turbine': 900
+    'wind_turbine': 900,
+    'fossil_generator': 2400,
+    'coal_generator': 9200,
+    'oil_generator': 21_300,
+    'gas_generator': 54_600
 }
